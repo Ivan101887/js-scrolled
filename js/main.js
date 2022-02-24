5e3db2608ca08bc8725f5cfcf6161b30b9066f7a
Ivan yuan <Ivan_Yuan@cmoney.com.tw>
2022-02-24 11:58:01 +0800

    array version

--- js/main.js
+++ js/main.js
@@ -3,16 +3,36 @@ const elemTitle = elemMain.querySelectorAll('.course__title');
 const elemDescWrap = elemMain.querySelectorAll('.course__descWrap');
 const elemImgContainer = elemMain.querySelectorAll('.course__imgContainer');
 const avalHeight = window.innerHeight;
-window.addEventListener('scroll', selection);
+const arrAllY = [];
+const arrAllel = [];
+let index = 0;
+window.addEventListener('scroll', handleAnimation);
+setArrs()
+function setArrs() {
+  elemTitle.forEach((e, i) => {
+    arrAllY.push(e.offsetTop);
+    arrAllel[i * 3] = elemTitle[i];
+  });
+  elemDescWrap.forEach((e, i) => {
+    arrAllY.push(e.offsetTop);
+    arrAllel[i * 3 + 1] = elemImgContainer[i];
+  });
+  elemImgContainer.forEach((e, i) => {
+    arrAllY.push(e.offsetTop);
+    arrAllel[i * 3 + 2] = elemDescWrap[i];
+  });
+  arrAllY.sort(function (a, b) { return a - b });
+}
 
 function handleAnimation(e) {
-  let currentPosY = e.getBoundingClientRect().top;
-  if (currentPosY <= (avalHeight * 1 / 2)) {
-    showCourse(e);
+  if (arrAllY[index] - window.scrollY <= (avalHeight * 1 / 2)) {
+    showCourse(arrAllel[index])
+    index += 1;
   } else {
-    hideCourse(e);
+    hideCourse(arrAllel[index])
   }
 }
+
 function selection() {
   elemTitle.forEach(handleAnimation);
   elemImgContainer.forEach(handleAnimation);
@@ -30,6 +50,7 @@ function showCourse(item) {
     item.classList.add('js-fade-left');
   }
 }
+
 function hideCourse(item) {
   if (item.classList.contains('js-fade-bottom')) {
     item.classList.add('js-fade-out');
